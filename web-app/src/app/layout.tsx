80b6f5d3bcf19e40b3a2bf3d3afb832d3caf016c
MattWong-ca <matthew.yt.wong@gmail.com>
2024-01-20 19:45:28 -0500

    Update title metadata

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Matthew Wong",
+  title: "Matthew Wong | Software Developer",
   description: "Matthew Wong | UWaterloo | Software Developer",
 };
 
