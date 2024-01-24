e1bb6b3e74728fcdb1f2a1f18734105056f542d4
MattWong-ca <matthew.yt.wong@gmail.com>
2024-01-23 21:13:45 -0500

    Update icons

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -24,12 +24,23 @@ export default function Home() {
               <div className="bg-black rounded-xl w-20 h-6 mx-auto"></div>
               <Image src="/statusbar.png" alt="iPhone Status Bar" width={80} height={50} className="filter brightness-0 invert justify-end" />
             </div>
-            <div className="pt-4">
+            <div className="pt-4 flex space-x-5 justify-center">
               <div className="flex flex-col items-center">
-              <Image src="/about.png" alt="About me icon" width={60} height={60} className="rounded-40"></Image>
-              <p className="text-white">About</p>
+                <Image src="/about.png" alt="About me icon" width={55} height={55} className="rounded-40"></Image>
+                <p className="text-xs text-white">About</p>
               </div>
-
+              <div className="flex flex-col items-center">
+                <Image src="/about.png" alt="About me icon" width={55} height={55} className="rounded-40"></Image>
+                <p className="text-xs text-white">About</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <Image src="/about.png" alt="About me icon" width={55} height={55} className="rounded-40"></Image>
+                <p className="text-xs text-white">About</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <Image src="/about.png" alt="About me icon" width={55} height={55} className="rounded-40"></Image>
+                <p className="text-xs text-white">About</p>
+              </div>
             </div>
           </div>
         </div>
