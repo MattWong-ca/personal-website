6929d9715c79a5a1363abf73b522cec51e2cc9b8
MattWong-ca <matthew.yt.wong@gmail.com>
2024-02-01 00:12:03 -0500

    Start bottom icon container, remove Tailwind soon

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -45,17 +45,18 @@ export default function Home() {
       <div className="bg-red-500 flex-1 flex items-center justify-center border-l-4 border-black">
         <div className="w-100 h-300 border-4 border-black rounded-70 flex items-start justify-center bg-cover pt-3"
           style={{ backgroundImage: `url('https://i.pinimg.com/736x/30/86/1b/30861b54751c4f458c0054fb500dc76e.jpg')` }}>
-          <div>
-            <div className="w-70 flex items-center">
+          <div className="relative">
+            <div /* Top status bar */ className="w-70 flex items-center">
               <p className="text-m text-white justify-start pr-5 pl-2">{currentTime}</p>
               <div className="bg-black rounded-xl w-20 h-6 mx-auto"></div>
               <Image src="/statusbar.png" alt="iPhone Status Bar" width={80} height={50} className="filter brightness-0 invert justify-end" />
             </div>
-            <div className="pt-8 pl-1 flex space-x-5">
+            <div /* Icons first row */ className="pt-8 pl-1 flex space-x-5">
               {icons.map((item, itemIndex) => (
                 <IconAndText key={itemIndex} src={item.src} text={item.text} />
               ))}
             </div>
+            <div className="absolute bottom-0 w-100 h-20 bg-black rounded-70"></div>
           </div>
         </div>
       </div>
