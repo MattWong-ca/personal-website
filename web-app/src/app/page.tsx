989ad04299f8b7bf0bca03b60437a927d7f55f82
MattWong-ca <matthew.yt.wong@gmail.com>
2024-02-15 23:49:35 -0500

    Add comments

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -54,33 +54,33 @@ export default function Home() {
     { src: '/education.jpeg', text: 'Education', link: 'https://www.linkedin.com/in/mattwong-ca/' }
   ];
   const [isPopupVisible, setPopupVisible] = useState(false);
-  const popupRef = useRef<HTMLDivElement | null>(null);
-  const iconRef = useRef<HTMLDivElement | null>(null);
+  // const popupRef = useRef<HTMLDivElement | null>(null);
+  // const iconRef = useRef<HTMLDivElement | null>(null);
 
   // Function to toggle the visibility of the popup and set its content
   const togglePopup = () => {
     setPopupVisible(!isPopupVisible);
   };
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      popupRef.current &&
-      !popupRef.current.contains(event.target as Node) &&
-      iconRef.current &&
-      !iconRef.current.contains(event.target as Node)
-    ) {
-      setPopupVisible(false);
-    }
-  };
+  // const handleClickOutside = (event: MouseEvent) => {
+  //   if (
+  //     popupRef.current &&
+  //     !popupRef.current.contains(event.target as Node) &&
+  //     iconRef.current &&
+  //     !iconRef.current.contains(event.target as Node)
+  //   ) {
+  //     setPopupVisible(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    // Add event listener on component mount
-    document.addEventListener('mousedown', handleClickOutside);
-    // Remove event listener on component unmount
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  // useEffect(() => {
+  //   // Add event listener on component mount
+  //   document.addEventListener('mousedown', handleClickOutside);
+  //   // Remove event listener on component unmount
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClickOutside);
+  //   };
+  // }, []);
 
   return (
     <div style={{ overflow: 'hidden', height: '100vh', display: 'flex', position: 'relative', zIndex: '100' }}>
@@ -160,24 +160,24 @@ export default function Home() {
               <IconAndText src={'/camera.webp'} link='https://www.linkedin.com/in/mattwong-ca/' />
               <IconAndText src={'/safari.webp'} link='https://www.google.com/search?q=matt+wong+waterloo' />
               <IconAndText src={'/phone.webp'} link='https://www.linkedin.com/in/mattwong-ca/' />
-              <div ref={iconRef} className="flex flex-col items-center">
+              {/* <div className="flex flex-col items-center">
                 <Image style={{ position: 'relative', zIndex: '1000' }} onClick={togglePopup} src={'/phone.webp'} alt="About me icon" width={55} height={55} className="rounded-40"></Image>
                 <p style={{ marginTop: '0px', fontSize: '11px', maxWidth: '55px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} className="apple text-white">{'text'}</p>
-              </div>
+              </div> */}
 
             </div>
           </div>
 
         </div>
       </div>
-      <div style={{ position: 'relative', zIndex: '100' }}>
-        {isPopupVisible && (<div ref={popupRef} style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: '999' }}>
+      {/* <div style={{ position: 'relative', zIndex: '100' }}>
+        {isPopupVisible && (<div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: '999' }}>
           <div style={{ width: '60%', height: '70%', backgroundColor: 'black', borderRadius: '10px', textAlign: 'center' }}>
             <div style={{ width: '100%', height: '20px', backgroundColor: 'grey', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }}></div>
             <p>This is the content of the centered component.</p>
           </div>
         </div>)}
-      </div>
+      </div> */}
 
 
 
