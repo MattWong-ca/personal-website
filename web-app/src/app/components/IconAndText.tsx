7c0c78107e07fea1389a963f7286b586a67d31f8
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-14 20:56:03 -0400

    Clean icon styling

--- web-app/src/app/components/IconAndText.tsx
+++ web-app/src/app/components/IconAndText.tsx
@@ -15,15 +15,16 @@ const IconAndText: React.FC<IconAndTextProps> = ({ src, text, link, onClick }) =
                 <a href={link} target="_blank" rel="noopener noreferrer">
                     <div onClick={onClick} className="flex flex-col items-center">
                         <Image src={src} alt="About me icon" width={52} height={52} className="rounded-40"></Image>
-                        {text ? (<p style={{ marginTop: '2px', fontSize: '11px', maxWidth: '55px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} className="apple text-white">{text}</p>
-                        ) : (null)}
+                        {text ?
+                            (<p className="apple text-white mt-0.5 truncate" style={{ fontSize: '10px', maxWidth: '55px' }}>{text}</p>) 
+                            : (null)}
                     </div>
                 </a>
             ) : (
                 <div onClick={onClick} className="flex flex-col items-center">
                     <Image src={src} alt="About me icon" width={52} height={52} className="rounded-40"></Image>
-                    {text ? (<p style={{ marginTop: '2px', fontSize: '10px', maxWidth: '55px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} className="apple text-white">{text}</p>
-                    ) : (null)}
+                    {text ? (<p className="apple text-white mt-0.5 truncate" style={{ fontSize: '10px', maxWidth: '55px' }}>{text}</p>) 
+                    : (null)}
                 </div>
             )}
         </div>
