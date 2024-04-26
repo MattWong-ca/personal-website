99ac08a1b42e3c037be61d714def2d6b2e0cd94f
MattWong-ca <matthew.yt.wong@gmail.com>
2024-04-25 23:33:52 -0400

    Update ExaCare notes

--- web-app/pages/writing/exacare.tsx
+++ web-app/pages/writing/exacare.tsx
@@ -16,12 +16,12 @@ const ExaCare = () => {
                     <Image
                         style={{ marginTop: '75px', marginBottom: '75px' }}
                         src="/exacare.png"
-                        alt="Matt Wong profile picture"
+                        alt="ExaCare logo"
                         width={500}
                         height={0}
                         className="mx-auto"
                     />
-                    <p className="italic" style={{ fontSize: '18px'}}>
+                    <p className="italic" style={{ fontSize: '18px' }}>
                         Founded in early 2022, ExaCare is a health tech startup building
                         an all-in-one EHR and CRM platform for senior care homes. They
                         raised $6.5M from Foundation Capital, 1984 Ventures, and Fractal
@@ -31,6 +31,36 @@ const ExaCare = () => {
                     <p className={`${title.className}`} style={{ marginTop: '50px', fontSize: '24px' }}>Backend - How to Make an API Endpoint</p>
                     <p className={`${garamond400.className}`} style={{ fontSize: '20px' }}>
                         Tech stack: serverless architecture, AWS Lambda, API Gateway, S3, Node.js, Sequelize, PostgresSQL, pgAdmin, Docker
+                    </p>
+                    <Image
+                        style={{ marginTop: '40px', marginBottom: '30px' }}
+                        src="/flowchart.png"
+                        alt="ExaCare flowchart"
+                        width={700}
+                        height={0}
+                        className="mx-auto"
+                    />
+                    <p className={`${garamond400.className}`} style={{ fontSize: '20px' }}>
+                        General flow:
+                    </p>
+
+                    <ol className={`${garamond400.className}`} style={{ fontSize: '20px' }}>
+                        <li>1. Define API endpoint</li>
+                        <li>2. Create handler code (eg. findAll)</li>
+                        <li>3. Define data models</li>
+                        <li>4. Create service class (calls repo class methods)</li>
+                        <li>5. Create repo class (methods interact with database)</li>
+                    </ol>
+
+                    <br />
+
+                    <p className={`${garamond400.className}`} style={{ fontSize: '20px' }}>
+                        The BE uses serverless cloud architecture through API Gateway and
+                        AWS Lambda. API Gateway is like a centralized control center that
+                        receives a request from the FE and directs it to the right service
+                        or Lambda function. Lambda is a compute service that executes your
+                        code in response to events like HTTP requests (hence eliminating
+                        the need to manage servers).
                     </p>
                 </div>
             </div>
