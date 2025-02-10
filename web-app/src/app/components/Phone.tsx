88efd0cf483d94f89dd46d5afcdb299771cb0718
MattWong-ca <matthew.yt.wong@gmail.com>
2025-02-10 18:23:44 +0800

    About Me update

--- web-app/src/app/components/Phone.tsx
+++ web-app/src/app/components/Phone.tsx
@@ -124,10 +124,10 @@ const Phone: React.FC<iPhoneProps> = ({
               </div>
               <p className={`${poppins600.className} mt-5 pb-2 text-black`} style={{ fontSize: '24px' }}>Currently</p>
               <div className={`${garamond400.className} text-black`} style={{ marginLeft: '1.3rem', fontSize: '22px' }}>
-                <p>➔&nbsp;&nbsp; Building Lingocaster</p>
+                <p>➔&nbsp;&nbsp; CS exchange at SUTD (Singapore)</p>
                 <p className="mt-2">➔&nbsp;&nbsp; Contributing to IPFS & Filecoin DevEx</p>
                 <p className="mt-2">➔&nbsp;&nbsp; Devcon Scholar via Ethereum Foundation</p>
-                <p className="mt-2">➔&nbsp;&nbsp; Building projects at hackathons</p>
+                <p className="mt-2">➔&nbsp;&nbsp; Building projects at with real users</p>
                 <p className="mt-2">➔&nbsp;&nbsp; Reading about Canadian startup founders</p>
                 <p className="mt-2">➔&nbsp;&nbsp; On a {codingStreak} day coding streak!</p>
               </div>
